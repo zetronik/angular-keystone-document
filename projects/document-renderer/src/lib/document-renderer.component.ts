63ae28b5138c5c519ea8b4303053dfaa7294b98f
andriyomelchenko <andriy.omelchenko@agilie.com>
2022-06-04 22:54:34 +0300

    heading and paragraph added style

--- projects/document-renderer/src/lib/document-renderer.component.ts
+++ projects/document-renderer/src/lib/document-renderer.component.ts
@@ -3,7 +3,6 @@ import {Component, Input, OnInit} from '@angular/core';
 @Component({
   selector: 'akd-document-renderer',
   templateUrl: 'document-renderer.component.html',
-  styles: ['document-renderer.component.scss']
 })
 export class DocumentRendererComponent implements OnInit {
 
